8b5e5b7be8a566ea37599f2cee114a269ffb2cd0
TatsianaMark <tanya030890@mail.ru>
2020-06-28 10:49:54 +0200

    Save before ejecting

--- src/App.js
+++ src/App.js
@@ -6,18 +6,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<a*/}
+          {/*className="App-link"*/}
+          {/*href="https://reactjs.org"*/}
+          {/*target="_blank"*/}
+          {/*rel="noopener noreferrer"*/}
+        {/*>*/}
+          {/*Learn React*/}
+        {/*</a>*/}
+          <div>
+              <p className="par">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta ducimus molestias quod unde. Earum impedit neque optio quibusdam suscipit? Atque doloremque eius excepturi iste nemo omnis rem similique! Enim.
+              </p>
+              <p className="parom">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta ducimus molestias quod unde. Earum impedit neque optio quibusdam suscipit? Atque doloremque eius excepturi iste nemo omnis rem similique! Enim.
+              </p>
+              <p className="paromas">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad dicta ducimus molestias quod unde. Earum impedit neque optio quibusdam suscipit? Atque doloremque eius excepturi iste nemo omnis rem similique! Enim.
+              </p>
+          </div>
       </header>
     </div>
   );
